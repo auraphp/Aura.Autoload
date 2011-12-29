2ba0799dfd56a78aa762f53cfe8623aede386cf6
Paul M. Jones <pmjones88@gmail.com>
2011-12-29 16:54:01 -0600

    add src and test files from last commit

---
+++ src/Aura/Autoload/Exception/NotDeclared.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 
+ * This file is part of the Aura project for PHP.
+ * 
+ * @license http://opensource.org/licenses/bsd-license.php BSD
+ * 
+ */
+namespace Aura\Autoload\Exception;
+
+/**
+ * 
+ * Indicates the loader did not find a class definition after loading a file.
+ * 
+ * @package Aura.Autoload
+ * 
+ */
+class NotDeclared extends \Aura\Autoload\Exception {}
---
+++ tests/Aura/Autoload/MockAutoloadUndeclared2.php
@@ -0,0 +1,6 @@
+<?php
+namespace Aura\Autoload;
+// note that the file name and class name do not match
+class MockAutoloadClassIsNotDeclaredProperly2 extends MockAutoloadClass {
+    
+}
