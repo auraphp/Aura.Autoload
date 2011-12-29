1b182dbc278d1ccf7f014a566a320aa1ad6673a1
Paul M. Jones <pmjones88@gmail.com>
2011-12-29 10:36:01 -0600

    update src.php

--- src.php
+++ src.php
@@ -1,4 +1,5 @@
 <?php
 require_once __DIR__ . '/src/Aura/Autoload/Loader.php';
 require_once __DIR__ . '/src/Aura/Autoload/Exception.php';
+require_once __DIR__ . '/src/Aura/Autoload/Exception/AlreadyLoaded.php';
 require_once __DIR__ . '/src/Aura/Autoload/Exception/NotFound.php';
