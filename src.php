ea57a7ff75ebcbc7ef6a31ccfd7fb4778fde9c8d
Paul M. Jones <pmjones88@gmail.com>
2011-12-29 18:11:34 -0600

    fix src.php

--- src.php
+++ src.php
@@ -3,4 +3,4 @@ require_once __DIR__ . '/src/Aura/Autoload/Loader.php';
 require_once __DIR__ . '/src/Aura/Autoload/Exception.php';
 require_once __DIR__ . '/src/Aura/Autoload/Exception/AlreadyLoaded.php';
 require_once __DIR__ . '/src/Aura/Autoload/Exception/NotReadable.php';
-require_once __DIR__ . '/src/Aura/Autoload/Exception/NotDefined.php';
+require_once __DIR__ . '/src/Aura/Autoload/Exception/NotDeclared.php';
